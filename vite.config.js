9d072ca0262dae0c895275b26bab791f4f1f03de
Tomas Montalvo <tomas.montalvo.contractor@openpay.com.es>
2025-04-01 18:17:13 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  build: {
+    rollupOptions: {
+      external: ['chart.js'], // Excluir chart.js del bundle
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
